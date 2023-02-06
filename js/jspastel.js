0449e4f9eae9cf884df875c5c8d3ed7fabad7101
Julio Alonso <chichejalonso@gmail.com>
2023-02-06 12:44:46 -0300

    primer escritura en repositorio

---
+++ js/jspastel.js
@@ -0,0 +1,7 @@
+// Aquí se pueden definir los pasteles y alfajores en forma de objetos
+const pasteles = [
+    {
+      nombre: "Pastel de chocolate",
+      imagen: "https://example.com/pastel-chocolate.jpg",
+      precio: 25.00
+    },]
